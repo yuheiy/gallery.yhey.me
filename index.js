2f3eed44380a93431108e64dfe6d0de5580395f2
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-02-07 13:49:40 +0900

    Clean

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const config = require('./config.json');
 
 const API_URL = 'https://getpocket.com/v3/get';
 
-request.post({url: API_URL, form: {
-  consumer_key: config.consumer_key,
-  access_token: config.access_token,
-  tag: config.tag
-}}, (err, res, body) => {
+request.post({
+  url: API_URL,
+  form: {
+    consumer_key: config.consumer_key,
+    access_token: config.access_token,
+    tag: config.tag
+  }
+}, (err, res, body) => {
   const data = JSON.parse(body);
   const list = sortby(data.list, o => -parseInt(o.time_updated, 10));
   const items = list.map(item => {
-    const title = item.resolved_title.replace('\n', ' ');
+    const title = (item.resolved_title || item.given_title).replace('\n', ' ');
     const url = item.resolved_url;
     return {
-      title: title.length > 40 ? `${title.slice(0, 40)}...` : title,
+      title: title.length > 40 ? `${title.substr(0, 40)}...` : title,
       url: url,
       thumbnail: `http://capture.heartrails.com/medium/delay=3?${url}`
     };
