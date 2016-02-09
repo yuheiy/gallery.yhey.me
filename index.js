f3b3b10246a5b1fad36f1b26d1eb134a4e4f4565
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-02-09 22:49:08 +0900

    Fix

--- index.js
+++ index.js
@@ -7,29 +7,34 @@ const config = require('./config.json');
 
 const API_URL = 'https://getpocket.com/v3/get';
 
-request.post({
-  url: API_URL,
-  form: {
-    consumer_key: config.consumer_key,
-    access_token: config.access_token,
-    tag: config.tag
-  }
-}, (err, res, body) => {
-  const data = JSON.parse(body);
-  const list = sortby(data.list, o => -parseInt(o.time_updated, 10));
-  const items = list.map(item => {
-    const title = (item.resolved_title || item.given_title).replace('\n', ' ');
-    const url = item.resolved_url;
-    return {
-      title: title.length > 40 ? `${title.substr(0, 40)}...` : title,
-      url: url,
-      thumbnail: `http://capture.heartrails.com/medium/delay=3?${url}`
-    };
-  });
+return new Promise(done => {
+  request.post({
+    url: API_URL,
+    form: {
+      consumer_key: config.consumer_key,
+      access_token: config.access_token,
+      tag: config.tag
+    }
+  }, (err, res, body) => {
+    const data = JSON.parse(body);
+    const list = sortby(data.list, o => -parseInt(o.time_updated, 10));
+    const items = list.map(item => {
+      const title = (item.resolved_title || item.given_title)
+        .replace('\n', ' ');
+      const url = item.resolved_url;
+      return {
+        title: title.length > 40 ? `${title.substr(0, 40)}...` : title,
+        url: url,
+        thumbnail: `http://capture.heartrails.com/medium/delay=3?${url}`
+      };
+    });
 
-  const html = jade.renderFile('src/index.jade', {
-    pretty: true,
-    items: items
+    const html = jade.renderFile('src/index.jade', {
+      pretty: true,
+      items: items
+    });
+    fs.writeFileSync('dist/index.html', html, 'utf8');
+
+    done();
   });
-  fs.writeFileSync('dist/index.html', html, 'utf8');
 });
