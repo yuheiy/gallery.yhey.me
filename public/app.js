f339f2455adde557ad1b4b82c86d370da6e67ee4
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-03-14 14:39:50 +0900

    Clean scripts

---
+++ public/app.js
@@ -0,0 +1,57 @@
+(function () {
+  'use strict';
+
+  var adjustGrid = function (elms) {
+    var i;
+    var l = elms.length;
+    var elm;
+    var windowWidth = window.innerWidth;
+    var minWidth = 320;
+    var maxWidth = windowWidth / Math.floor(windowWidth / minWidth);
+
+    for (i = 0; i < l; i++) {
+      elm = elms[i];
+      elm.style.maxWidth = maxWidth + 'px';
+    }
+  };
+
+  var delayLoading = function (elms) {
+    var i;
+    var l = elms.length;
+    var elm;
+    var offsetTop;
+    var scrollTop = window.pageYOffset;
+    var scrollBottom = scrollTop + window.innerHeight;
+    var image;
+
+    for (i = 0; i < l; i++) {
+      elm = elms[i];
+      offsetTop = elm.getBoundingClientRect().top + scrollTop;
+
+      if (offsetTop > scrollTop && offsetTop < scrollBottom) {
+        image = elm.dataset.image;
+        elm.style.backgroundImage = 'url(' + image + ')';
+      }
+    }
+  };
+
+  var init = function () {
+    var items = document.querySelectorAll('.list li');
+
+    adjustGrid = adjustGrid.bind(null, items);
+    delayLoading = delayLoading.bind(null, items);
+
+    adjustGrid();
+    delayLoading();
+
+    window.addEventListener('resize', adjustGrid);
+    window.addEventListener('scroll', delayLoading);
+    window.addEventListener('resize', delayLoading);
+  };
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
