1c659f59c62c90a478279fa851b60cb2a6e18562
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-03-15 14:11:08 +0900

    Build

--- adjustGrid.js
+++ adjustGrid.js
@@ -1,13 +1,45 @@
 (function (w, d) {
   'use strict';
 
+  var filter = function (arrayLike, cb) {
+    var i;
+    var l = arrayLike.length;
+    var item;
+    var result = {
+      matched: [],
+      unmatched: []
+    };
+
+    for (i = 0; i < l; i++) {
+      item = arrayLike[i];
+
+      if (cb(item, i)) {
+        result.matched.push(item);
+      } else {
+        result.unmatched.push(item);
+      }
+    }
+
+    return result;
+  };
+
   var adjustGrid = function (elms) {
+    var l = elms.length;
     var windowWidth = w.innerWidth;
     var minWidth = 320;
-    var maxWidth = windowWidth / Math.floor(windowWidth / minWidth);
+    var col = Math.floor(windowWidth / minWidth);
+    var overIndex = l - l % col;
+    var maxWidth = windowWidth / col;
+    var filtered = filter(elms, function (elm, i) {
+      var isOver = i + 1 > overIndex;
+      return isOver;
+    });
 
-    Array.prototype.forEach.call(elms, function (elm) {
+    filtered.matched.forEach(function (elm) {
       elm.style.maxWidth = maxWidth + 'px';
+    });
+    filtered.unmatched.forEach(function (elm) {
+      elm.style.maxWidth = '';
     });
   };
 
