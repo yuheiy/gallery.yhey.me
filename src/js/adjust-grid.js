a910ff6d8a438962aa94d5760efd3285a4495a27
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2017-04-21 01:13:31 +0900

    use grid instead of flexbox

--- src/js/adjust-grid.js
+++ src/js/adjust-grid.js
@@ -1,13 +1,7 @@
-// adjust grid of flexbox
-
 const adjustGrid = list => {
-  const gridCols = list.children
-  const gridColCount = gridCols.length
   const windowWidth = window.innerWidth
-  const minWidth = Number(
-    window.getComputedStyle(gridCols[0]).minWidth.replace(/px$/, '')
-  )
-  const colCountPerRow = Math.floor(windowWidth / minWidth)
+  const minWidth = 320
+  const colCountPerRow = Math.floor(windowWidth / minWidth) || 1
 
   list.style.setProperty('--col-count-per-row', colCountPerRow)
 }
